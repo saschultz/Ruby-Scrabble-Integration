d19e253409b29a41dca39e875d4674996f0c8431
James Higgins and Tyler Stephenson <tyler@>
2017-04-18 13:12:42 -0700

    refactor white space

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -3,9 +3,7 @@ require('descriptive_statistics')
 class String
   define_method(:scrabble) do
     letter_values = Hash.new()
-    letter_values.store(
-      ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"], 1
-    )
+    letter_values.store(["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"], 1)
     letter_values.store(["d", "g"], 2)
     letter_values.store(["b", "c", "m", "p"], 3)
     letter_values.store(["f", "h", "v", "w", "y"], 4)
