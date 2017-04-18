bba9aac74d7f847c1adb21b620a7b7b5e1960e39
James Higgins and Tyler Stephenson <james@>
2017-04-18 09:25:44 -0700

    add second test and get working

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,15 +1,52 @@
 class String
   define_method(:scrabble) do
     letter_values = Hash.new()
-    letter_values.store(
-      ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"], 1
-    )
-    letter_values.store(["d", "g"], 2)
-    letter_values.store(["b", "c", "m", "p"], 3)
-    letter_values.store(["f", "h", "v", "w", "y"], 4)
+    letter_values.store("a", 1)
+    letter_values.store("e", 1)
+    letter_values.store("i", 1)
+    letter_values.store("0", 1)
+    letter_values.store("u", 1)
+    letter_values.store("l", 1)
+    letter_values.store("n", 1)
+    letter_values.store("r", 1)
+    letter_values.store("s", 1)
+    letter_values.store("t", 1)
+    letter_values.store("d", 2)
+    letter_values.store("g", 2)
+    letter_values.store("b", 3)
+    letter_values.store("c", 3)
+    letter_values.store("m", 3)
+    letter_values.store("p", 3)
+    letter_values.store("f", 4)
+    letter_values.store("h", 4)
+    letter_values.store("v", 4)
+    letter_values.store("w", 4)
+    letter_values.store("y", 4)
     letter_values.store("k", 5)
-    letter_values.store(["j", "x"], 8)
-    letter_values.store(["q", "z"], 10)
-    letter_values.fetch(self)
+    letter_values.store("j", 8)
+    letter_values.store("x", 8)
+    letter_values.store("q", 10)
+    letter_values.store("z", 10)
+    # letter = self.downcase
+    # hash_key = letter_values.each do |key|
+      # if key.include?(letter) == true
+      #   key
+      # end
+      letter_values.fetch(self)
+    end
   end
-end
+
+
+
+
+
+#
+# letter_values.store(
+#   ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"], 1
+# )
+# letter_values.store(["d", "g"], 2)
+# letter_values.store(["b", "c", "m", "p"], 3)
+# letter_values.store(["f", "h", "v", "w", "y"], 4)
+# letter_values.store("k", 5)
+# letter_values.store(["j", "x"], 8)
+# letter_values.store(["q", "z"], 10)
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -2,7 +2,14 @@ require('rspec')
 require('scrabble')
 
 describe('String#scrabble') do
-  it('returns a scrabble score for a letter') do
+  it('returns a scrabble score for a letter not in an array') do
     expect("k".scrabble()).to(eq(5))
+  end
+
+  it('returns a scrabble score for a letter in an array') do
+    expect("a".scrabble()).to(eq(1))
+  end
+  it('returns a scrabble score for multiple letters in an word') do
+    expect("abc".scrabble()).to(eq(6))
   end
 end
